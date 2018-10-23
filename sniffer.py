1d8f019549acf14bf067866aeae37ad261f49f00
Christophe CANTELLA <cantella.christophe@gmail.com>
2018-10-23 10:34:31 -0400

    Récupération du l'adresse ip active

--- sniffer.py
+++ sniffer.py
@@ -17,7 +17,8 @@ def signal_handler(signal, frame):
 
 if os.name == "nt":
     s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
-    s.bind(("YOUR_INTERFACE_IP",0))
+    s.bind((local_ip,0))
+    print("Monitoring de l\' adresse %s" % local_ip)
     s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL,1)
     s.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 else:
@@ -38,21 +39,29 @@ while True:
         break
 
     eth = unpack.eth_header(packet[0:14])
+    print(eth)
     iph = unpack.ip_header(packet[14:34])
-
+    print(iph)
+    #tph = unpack.tcp_header(packet[34:54])
+    #print(tph)
 
     if iph.get("Protocol") == 6 :
         tcph = unpack.tcp_header(packet[34:54])
         compteur.add_tcp(tcph.get("Destination Port"), iph.get("Source Address"))
+        print("version 6")
     elif iph.get("Protocol") == 17 :
         udph = unpack.udp_header(packet[34:42])
         compteur.add_udp(udph.get("Destination Port"), iph.get("Source Address"))
+        print("version 17")
     elif iph.get("Protocol") == 1 :
         icmph = unpack.icmp_header(packet[34:42])
         compteur.add_icmp(iph.get("Source Address"))
+        print("version 1")
+    else :
+        print("ne matche rien")
 
     if interrupted:
-        print("Ha ! (cf: D.Brognard)")
+        print("Terminated")
         break
 
-# TODO : BLBLB sort tableau et print et fichier et sushi
+# TODO : BLBLB sort tableau et print et fichier
